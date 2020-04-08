9caf4e6c24bc130804034b5cb95fe384bc4742da
Edgar Hernandez <edgar.hernandez_@outlook.com>
2020-04-08 17:43:05 -0500

    Update main.tf

--- s3-example/main.tf
+++ s3-example/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "private" {
+resource "aws_s3_bucket" "buckets" {
   count =2
   bucket = "${var.bucket}-${count.index}"
   acl = "private"
@@ -6,13 +6,21 @@ resource "aws_s3_bucket" "private" {
 
 variable "bucket" {
   description = "S3 bucket name"
-  type = "string"
+  type = "list"
 }
 
 provider "aws" {
   version = "~> 2.0"
 }
 
-output "buckets" {
-  value = "aws_s3_bucket.private.bucket.*.bucket"
+output "bucket_0" {
+  value = "${aws_s3_bucket.buckets.*.id[0]}"
+}
+
+output "bucket_1" {
+  value = "${aws_s3_bucket.buckets.*.id[1]}"
+}
+
+output "bucket_names" {
+  value = "${aws_s3_bucket.buckets.*.bucket}"
 }
